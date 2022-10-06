b33ea160a5c0ae8437380350464b1719f1e73c6d
Marat <Shagiakhmetov2499@gmail.com>
2022-10-06 10:29:54 +0300

    Fixed constraints, added methods for run animations

--- Repetition_2.9/ViewControllers/AnimationViewController.swift
+++ Repetition_2.9/ViewControllers/AnimationViewController.swift
@@ -17,11 +17,11 @@ class AnimationViewController: UIViewController {
     private let cornerForView: CGFloat = 10
     private let cornerForButton: CGFloat = 10
     
-    private let animation = Animation.getAnimation().animation
-    private let curve = Animation.getAnimation().curve
-    private let force = Animation.getAnimation().force
-    private let duration = Animation.getAnimation().duration
-    private let delay = Animation.getAnimation().delay
+    private var animation = Animation.getAnimation().animation
+    private var curve = Animation.getAnimation().curve
+    private var force = CGFloat(Animation.getAnimation().force)
+    private var duration = CGFloat(Animation.getAnimation().duration)
+    private var delay = CGFloat(Animation.getAnimation().delay)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,10 +38,45 @@ class AnimationViewController: UIViewController {
         labelOfResults.text = """
         Animation: \(animation)
         Curve: \(curve)
-        Force: \(force)
-        Duration: \(duration)
-        Delay: \(delay)
+        Force: \(format(from: force))
+        Duration: \(format(from: duration))
+        Delay: \(format(from: delay))
         """
+    }
+    
+    @IBAction func buttonRunAnimation() {
+        if buttonRun.currentTitle == "Run" {
+            runAnimation()
+        } else {
+            labelOfResults.text = """
+            Animation: \(animation)
+            Curve: \(curve)
+            Force: \(format(from: force))
+            Duration: \(format(from: duration))
+            Delay: \(format(from: delay))
+            """
+            runAnimation()
+        }
+    }
+    
+    private func runAnimation() {
+        viewOfAnimation.animation = animation
+        viewOfAnimation.curve = curve
+        viewOfAnimation.force = force
+        viewOfAnimation.duration = duration
+        viewOfAnimation.delay = delay
+        viewOfAnimation.animate()
+        animation = Animation.getAnimation().animation
+        curve = Animation.getAnimation().curve
+        force = CGFloat(Animation.getAnimation().force)
+        duration = CGFloat(Animation.getAnimation().duration)
+        delay = CGFloat(Animation.getAnimation().delay)
+        
+        buttonRun.setTitle("Run \(animation)", for: .normal)
+    }
+    
+    private func format(from text: CGFloat) -> String {
+        String(format: "%.2f", text)
     }
 }
 
